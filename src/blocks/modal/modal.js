dcbcf291335187fb378725603bb2037da26809f2
Дмитрий Малофеев <dim2k2006@yandex.ru>
2017-07-06 23:44:53 +0300

    Add block modal

---
+++ src/blocks/modal/modal.js
@@ -0,0 +1,60 @@
+(() => {
+    if (!window.app) {
+        window.app = {};
+    }
+
+    if (!('modules' in window.app)) {
+        window.app.modules = {};
+    }
+
+    if ('modal' in app.modules) {
+        return;
+    }
+
+    /**
+     * Creates a new Modal class.
+     * @class
+     */
+    const Modal = function() {
+        const self = this;
+
+        /**
+         * Get options for module
+         */
+        self.getOptions = () => {
+            self.container = document.querySelector('.modal');
+            self.closeBtn = document.querySelector('.modal__close');
+        };
+
+        /**
+         * Add events listeners
+         */
+        self.setupListener = () => {
+            self.closeBtn.addEventListener('click', self.close);
+        };
+
+        /**
+         * Open modal window
+         */
+        self.open = () => {
+            self.container.classList.add('modal_state_open');
+        };
+
+        /**
+         * Close modal window
+         */
+        self.close = () => {
+            self.container.classList.remove('modal_state_open');
+        };
+
+        /**
+         * Init module
+         */
+        self.init = () => {
+            self.getOptions();
+            self.setupListener();
+        };
+    };
+
+    window.app.modules.modal = new Modal();
+})();
