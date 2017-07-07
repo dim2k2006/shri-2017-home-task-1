8827ae977a532ccc7ac55157b36c461e473f5f88
Дмитрий Малофеев <dim2k2006@yandex.ru>
2017-07-07 18:09:06 +0300

    Update styles in block galleryItem

--- src/blocks/gallery/gallery.js
+++ src/blocks/gallery/gallery.js
@@ -23,6 +23,9 @@
          */
         self.getOptions = () => {
             self.container = document.querySelector('.gallery');
+            self.content = document.querySelector('.gallery__content');
+            self.data = JSON.parse(self.container.getAttribute('data-gallery')) || [];
+            self.template = document.querySelector('#templateDefault').innerHTML;
             self.modalOpen = app.modules.modal.open;
         };
 
@@ -39,7 +42,7 @@
          */
         self.handleClick = function(event) {
             const target = event.target;
-            const link = target.closest('.gallery__link');
+            const link = target.closest('.galleryItem__link');
 
             event.preventDefault();
 
@@ -47,9 +50,48 @@
                 return;
             }
 
-            const info = JSON.parse(link.getAttribute('data-info'));
+            const id = link.id;
 
-            self.modalOpen(info);
+            console.log(id);
+
+            // const info = JSON.parse(link.getAttribute('data-info'));
+
+            // self.modalOpen(info);
+        };
+
+        /**
+         * Render gallery
+         */
+        self.render = () => {
+            const list = document.createElement('ul');
+
+            list.classList.add('gallery__list');
+
+            self.data.forEach(galleryItem => {
+                const listItem = document.createElement('li');
+
+                listItem.classList.add('gallery__item');
+                listItem.innerHTML = self.template;
+
+                const link = listItem.querySelector('.galleryItem__link');
+                const img = listItem.querySelector('.galleryItem__img');
+                const title = listItem.querySelector('.galleryItem__title');
+
+                link.href = galleryItem.source;
+                link.title = galleryItem.title;
+                link.id = galleryItem.id;
+
+                img.src = galleryItem.preview;
+                img.alt = galleryItem.title;
+
+                title.textContent = galleryItem.title;
+
+                list.appendChild(listItem);
+            });
+
+            self.content.innerHTML = '';
+
+            self.content.appendChild(list);
         };
 
         /**
@@ -57,6 +99,7 @@
          */
         self.init = () => {
             self.getOptions();
+            self.render();
             self.setupListener();
         };
     };
